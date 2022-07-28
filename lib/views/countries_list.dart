ab910002b72f49e23af839fa950ce0a87fe1dd1f
kamran saleem <freinds221@gmail.com>
2022-07-28 18:13:17 +0500

    d

--- lib/views/countries_list.dart
+++ lib/views/countries_list.dart
@@ -1,5 +1,7 @@
 import 'package:covid_tracker_app/services/remote_services.dart';
+import 'package:covid_tracker_app/views/details.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class CountriesList extends StatefulWidget {
   const CountriesList({Key? key}) : super(key: key);
@@ -9,6 +11,7 @@ class CountriesList extends StatefulWidget {
 }
 
 class _CountriesListState extends State<CountriesList> {
+  TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     RemoteService remoteService = RemoteService();
@@ -17,32 +20,111 @@ class _CountriesListState extends State<CountriesList> {
         elevation: 0,
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  hintText: 'Search City',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                onChanged: (value) {
+                  setState(() {});
+                },
+                controller: textEditingController,
+                decoration: InputDecoration(
+                    hintText: 'Search City',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+              ),
             ),
-          ),
-          Expanded(
-              child: FutureBuilder(
-                  future: remoteService.fetchCountryStats(),
-                  builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-                    if (!snapshot.hasData) {
-                      return Text('Loading');
-                    } else {
-                      return ListView.builder(itemBuilder: ((context, index) {
-                        return Column(
-                          children: [],
-                        );
-                      }));
-                    }
-                  }))
-        ],
+            FutureBuilder(
+                future: remoteService.fetchCountryStats(),
+                builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Expanded(
+                      flex: 1,
+                      child: SpinKitFadingCircle(
+                        color: Colors.white,
+                        
+                      ));;
+                  } else {
+                    return Expanded(
+                      child: ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: ((context, index) {
+                            String name = snapshot.data![index]['country'];
+                            if (textEditingController.text.isEmpty) {
+                              return Column(
+                                children: [
+                                  InkWell(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => DetailPage(
+                                                  name: snapshot.data![index]
+                                                      ['country'],
+                                                  image: snapshot.data![index]
+                                                      ['countryInfo']['flag'],
+                                                  active: snapshot.data![index]
+                                                      ['active'],
+                                                  criticle: snapshot.data![index]
+                                                      ['critical'],
+                                                  test: snapshot.data![index]
+                                                      ['tests'],
+                                                  todayRecovered:
+                                                      snapshot.data![index]
+                                                          ['todayRecovered'],
+                                                  totalCase: snapshot.data![index]
+                                                      ['cases'],
+                                                  totalDeath: snapshot.data![index]
+                                                      ['deaths'],
+                                                  totalRecovered: snapshot.data![index]['recovered'])));
+                                    },
+                                    child: ListTile(
+                                      leading: Image(
+                                        height: 50,
+                                        width: 50,
+                                        image: NetworkImage(
+                                            snapshot.data![index]['countryInfo']
+                                                ['flag']),
+                                      ),
+                                      title: Text(
+                                          snapshot.data![index]['country']),
+                                      subtitle: Text(snapshot.data![index]
+                                              ['cases']
+                                          .toString()),
+                                    ),
+                                  )
+                                ],
+                              );
+                            } else if (name.toLowerCase().contains(
+                                textEditingController.text.toLowerCase())) {
+                              return Column(
+                                children: [
+                                  ListTile(
+                                    leading: Image(
+                                      height: 50,
+                                      width: 50,
+                                      image: NetworkImage(snapshot.data![index]
+                                          ['countryInfo']['flag']),
+                                    ),
+                                    title:
+                                        Text(snapshot.data![index]['country']),
+                                    subtitle: Text(snapshot.data![index]
+                                            ['cases']
+                                        .toString()),
+                                  )
+                                ],
+                              );
+                            } else {
+                              return Container();
+                            }
+                          })),
+                    );
+                  }
+                })
+          ],
+        ),
       ),
     );
   }
---
+++ lib/views/details.dart
@@ -0,0 +1,90 @@
+import 'package:covid_tracker_app/services/utitiles/widgets.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  final String name;
+  final String image;
+  final int totalCase,
+      totalRecovered,
+      totalDeath,
+      active,
+      test,
+      criticle,
+      todayRecovered;
+  const DetailPage(
+      {Key? key,
+      required this.name,
+      required this.image,
+      required this.active,
+      required this.criticle,
+      required this.test,
+      required this.todayRecovered,
+      required this.totalCase,
+      required this.totalDeath,
+      required this.totalRecovered})
+      : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: Text(widget.name),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(
+                      MediaQuery.of(context).size.height * 0.067),
+                  child: Card(
+                    child: Column(children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.067,
+                      ),
+                      ReuseableRow(
+                          title: 'Total cases',
+                          data: widget.totalCase.toString()),
+                      ReuseableRow(
+                          title: 'Active cases',
+                          data: widget.active.toString()),
+                      ReuseableRow(
+                          title: 'Criticle ', data: widget.criticle.toString()),
+                      ReuseableRow(
+                          title: 'Today Recovered',
+                          data: widget.todayRecovered.toString()),
+                      ReuseableRow(
+                          title: 'Total Recovered',
+                          data: widget.totalRecovered.toString()),
+                      ReuseableRow(
+                          title: 'Total Deaths',
+                          data: widget.totalDeath.toString()),
+                      ReuseableRow(
+                          title: 'Total Tests', data: widget.test.toString()),
+                    ]),
+                  ),
+                ),
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(widget.image),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
